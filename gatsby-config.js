d25c229e13776dd84dbd45f9f2deecfb8548d936
Suman Shrestha <summonshr@gmail.com>
2020-02-22 16:13:25 +0545

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'StockNp.com',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Start your journey as investor in Nepalese Stock Market.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
